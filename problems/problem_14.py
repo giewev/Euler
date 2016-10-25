e6d70215197345e7502a10602cbbe4083caeff3a
Ian Fade <giewev@gmail.com>
2016-10-24 20:10:33 -0400

    Fix typo

--- problems/problem_14.py
+++ problems/problem_14.py
@@ -18,7 +18,7 @@ def collatz_length(n, cache):
 	return cache[n]
 
 collatz_length_cache = { 0 : 0, 1 : 0 }
-upper_count = 1000000
-lower_bound = int(upper_count / 2) # Any number N less than half the limit would be bested by 2N
-path_lengths = [collatz_length(x, collatz_length_cache) for x in range(lower_bound, upper_count)]
+upper_bound = 1000000
+lower_bound = int(upper_bound / 2) # Any number N less than half the limit would be bested by 2N
+path_lengths = [collatz_length(x, collatz_length_cache) for x in range(lower_bound, upper_bound)]
 print(path_lengths.index(max(path_lengths)) + lower_bound)
